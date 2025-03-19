9cb90cf1b60077bf67d42232e90f0015a34f52e3
Oleh Kozhushko <oleg.k.linkedin@gmail.com>
2025-03-19 16:49:35 +0100

    Updated test config

--- src/main/java/mobile/screens/JoomHomeScreen.java
+++ src/main/java/mobile/screens/JoomHomeScreen.java
@@ -21,7 +21,7 @@ public class JoomHomeScreen extends BaseScreen {
     }
 
     public void waitUntilClickable(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
