6804930603b6f11c3bb6e40c7db6ca1e9f093531
Oleh Kozhushko <oleg.k.linkedin@gmail.com>
2025-04-14 20:03:44 +0200

    Test commit

--- src/main/java/mobile/locators/JoomHomePageLocators.java
+++ src/main/java/mobile/locators/JoomHomePageLocators.java
@@ -6,4 +6,5 @@ public class JoomHomePageLocators {
     public static final String PROFILE_BTN = "(//android.view.ViewGroup[@resource-id='com.joom:id/container'])[6]";
     public static final String SPIN_WHEEL_BTN = "//android.widget.Button";
     public static final String BACK_HOME_SCREEN_BTN = "//android.widget.Button";
+    //some changes
 }
